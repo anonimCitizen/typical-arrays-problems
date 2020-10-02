64178f7f560139c960cfa836f9b94f4296e85f2a
anonimCitizen <nno918318@gmail.com>
2020-10-02 23:36:59 +0300

    fix: max, min, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
+if (array == null || array.length == 0)
   return 0;
+else
+  return Math.min.apply(Math, array);
+  
 }
 
 exports.max = function max (array) {
+  if (array == null || array.length == 0)
   return 0;
+  else
+  return Math.max.apply(Math, array);
 }
 
 exports.avg = function avg (array) {
+  let sum = 0;
+  if (array == null || array.length == 0)
   return 0;
+
+  for (let i = 0; i < array.length; i++){
+    sum = sum + array[i];
+  }
+  return sum/array.length;
 }
+
+
